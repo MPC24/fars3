8a64f0472383df826bc910a1e084e6882d920951
MPC24 <mpconway@gmail.com>
2017-11-14 13:05:46 -0600

    no file fix

--- tests/testthat/test-sim.R
+++ tests/testthat/test-sim.R
@@ -1,12 +1,11 @@
 context("Test the file compisition of the data")
 
-library(dplyr)
-library(maps)
 
 
+test_that("fars_read() works properly", {
 
-test_that("fars_read() works correctly", {
-  acc <- system.file("extdata", "accident_2013.csv.bz2", package="fars3")
+  file <- "accident_2013.csv.bz2"
+  acc <- system.file("extdata", file, package = "fars3")
   acc <- fars_read(acc)
   expect_equal(nrow(acc), 30202)
   expect_is(acc,"data.frame")
