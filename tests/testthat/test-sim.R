3cc2d82b1013b98d3900c97b5122d18701b9340e
MPC24 <mpconway@gmail.com>
2017-11-14 11:31:31 -0600

    no file fix

--- tests/testthat/test-sim.R
+++ tests/testthat/test-sim.R
@@ -1,4 +1,4 @@
-context("Test the basic functionality of the package")
+context("Test the file compisition of the data")
 
 library(dplyr)
 library(maps)
@@ -6,19 +6,10 @@ library(maps)
 
 
 test_that("fars_read() works correctly", {
-  expect_is(fars_read(make_filename(2015)), "tbl_df")
-  expect_error(fars_read(make_filename(2016)))
+  acc <- system.file("extdata", "accident_2013.csv.bz2", package="fars3")
+  acc <- fars_read(acc)
+  expect_equal(nrow(acc), 30202)
+  expect_is(acc,"data.frame")
 })
 
-test_that("fars_summarize_years() works correctly", {
-  expect_is(fars_summarize_years(2013:2015), "tbl_df")
-  expect_is(fars_summarize_years(list(2013, 2014)), "tbl_df")
-  expect_equal(names(fars_summarize_years(2013:2015)), c("MONTH", 2013:2015))
-  expect_error(fars_summarize_years(2016))
-})
 
-test_that("fars_map_state() works correctly", {
-  expect_silent(fars_map_state(1, 2015))
-  expect_error(fars_map_state(100, 2015))
-  expect_error(fars_map_state(1, 2016))
-})
